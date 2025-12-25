b6f7f8c28a9b87cc424b319d128c46e9cff6ec79
조현덕 <gusejr4547@gmail.com>
2025-12-25 23:21:58 +0900

    feat: 뱀 몸통 세그먼트 프리팹 추가, 길이 4 뱀 이동

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SnakeController : MonoBehaviour
 {
@@ -7,10 +8,17 @@ public class SnakeController : MonoBehaviour
     // SerializeField makes private fields visible in the Unity Inspector
     [SerializeField] private float moveInterval = 0.1f; // Move every 0.1 seconds
 
+    // 몸통
+    [SerializeField] private Transform segmentPrefab;
+    [SerializeField] private int snakeLength = 4; // 머리 포함 길이
+    private List<Transform> segments = new List<Transform>(); // 몸통 조각 관리
+
+
     // Use this for initialization
     void Start()
     {
         Debug.Log("SnakeController has started.");
+        Setup();
         StartCoroutine(MoveLoop());
     }
 
@@ -30,7 +38,7 @@ public class SnakeController : MonoBehaviour
                 moveDirection = Vector2Int.down;
             }
         }
-        else if(moveDirection.y != 0)
+        else if (moveDirection.y != 0)
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
@@ -48,15 +56,43 @@ public class SnakeController : MonoBehaviour
         transform.position += new Vector3(moveDirection.x, moveDirection.y, 0f);
     }
 
-
     private IEnumerator MoveLoop()
     {
         var wait = new WaitForSeconds(moveInterval);
         while (true)
         {
-            MoveSnake();
+            MoveSegments();
             yield return wait;
         }
     }
 
+    private void Setup()
+    {
+        // 초기 몸통 설정
+        segments.Add(this.transform); // 머리 추가
+        for (int i = 0; i < snakeLength - 1; i++)
+        {
+            AddSegment();
+        }
+    }
+
+    private void AddSegment()
+    {
+        // 몸통 조각 추가
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    private void MoveSegments()
+    {
+        // 몸통 조각 이동
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
+        // 머리 이동
+        transform.position += new Vector3(moveDirection.x, moveDirection.y, 0f);
+    }
 }
